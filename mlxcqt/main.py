002a79ca0f6a4fd03d2c7c493f90b492f0df4101
Jonas Wielicki <j.wielicki@sotecware.net>
2015-08-02 23:44:16 +0200

    Change xdg*open interface for better usability with RESOURCE constant

--- mlxcqt/main.py
+++ mlxcqt/main.py
@@ -4,16 +4,33 @@ import aioxmpp.structs
 
 import mlxc.client
 
+from . import Qt
+from .ui.roster import Ui_roster_window
+
+
+class RosterWindow(Qt.QMainWindow, Ui_roster_window):
+    def __init__(self, main):
+        super().__init__()
+
+        self._main = main
+
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        result = super().closeEvent(event)
+        self._main._done_future.set_result(0)
+        return result
+
 
 class MLXCQt:
     def __init__(self, event_loop):
         self._loop = event_loop
         self._client = mlxc.client.Client()
+        self._roster = RosterWindow(self)
+        self._done_future = asyncio.Future()
 
     @asyncio.coroutine
     def run(self):
         self._client.load_state()
-        self._client.set_global_presence(aioxmpp.structs.PresenceState(True))
-        yield from asyncio.sleep(1)
-        yield from self._client.stop_and_wait_for_all()
-        self._client.save_state()
+        self._roster.show()
+        return (yield from self._done_future)
