f94a0e1069a259005a65dfdb48052031f49c650d
Jonas Wielicki <j.wielicki@sotecware.net>
2015-08-23 16:07:16 +0200

    Get rid of stupid test code

--- mlxcqt/main.py
+++ mlxcqt/main.py
@@ -186,23 +186,9 @@ class MLXCQt:
         self.roster = RosterWindow(self)
 
     @asyncio.coroutine
-    def test_foo(self):
-        dlg = Qt.QMessageBox(Qt.QMessageBox.Warning, "foo", "bar")
-        try:
-            print("result {}".format((yield from utils.exec_async(dlg))))
-        except asyncio.CancelledError:
-            print("cancelled")
-
-    @asyncio.coroutine
     def run(self, main_future):
         self.client.load_state()
         self.roster.show()
-
-        task = asyncio.async(self.test_foo())
-        task.add_done_callback(utils.asyncified_done)
-        yield from asyncio.sleep(1)
-        task.cancel()
-
         yield from main_future
         self.client.save_state()
 
