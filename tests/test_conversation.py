d311d1f79088a4f48d3552dade532b00a59d81ba
Jonas Wielicki <j.wielicki@sotecware.net>
2018-03-08 11:29:34 +0100

    Add a few more tests for the JavaScript stuff

--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -3,6 +3,9 @@ import logging
 import unittest
 import unittest.mock
 
+from datetime import datetime
+
+import lxml.html.html5parser
 import lxml.etree as etree
 
 import aioxmpp
@@ -42,11 +45,160 @@ class TestMessageViewPage(XMLTestCase):
     @asyncio.coroutine
     def _obtain_html(self):
         html_str = yield from self.page.channel.request_html()
-        return etree.fromstring(html_str)
+        return lxml.html.html5parser.fromstring(html_str)
 
     def test_basic_html(self):
         self.assertSubtreeEqual(
             run_coroutine(self._obtain_html(), timeout=20),
-            etree.fromstring("""<div id="messages"/>"""),
+            etree.fromstring(
+                """<div xmlns="http://www.w3.org/1999/xhtml" id="messages"/>"""
+            ),
+            ignore_surplus_attr=True,
+        )
+
+    def test_add_message(self):
+        self.page.channel.on_message.emit(
+            {
+                "timestamp": datetime(2018, 3, 8, 11, 16, 10).isoformat() + "Z",
+                "from_self": False,
+                "from_jid": "romeo@montague.lit",
+                "display_name": "Romeo Montague",
+                "color_full": "#123456",
+                "color_weak": "#123",
+                "attachments": [],
+                "body": "<em>test</em>",
+                "message_uid": "message-1"
+            }
+        )
+        self.assertSubtreeEqual(
+            etree.fromstring(
+                '<div xmlns="http://www.w3.org/1999/xhtml" id="messages">'
+                '<div class="message-block">'
+                '<div class="avatar"><img/></div>'
+                '<div class="from">Romeo Montague</div>'
+                '<div class="message-block-messages">'
+                '<div class="message">'
+                '<div class="timestamp">3/8/2018, 11:16:10 AM</div>'
+                '<div class="body"><div><em>test</em></div></div>'
+                '</div>'
+                '</div>'
+                '<div class="clearfix"></div>'
+                '</div>'
+                '</div>'
+            ),
+            run_coroutine(self._obtain_html(), timeout=20),
+            ignore_surplus_attr=True,
+        )
+
+    def test_fancy_time_formatting_on_subsequent_messages(self):
+        self.page.channel.on_message.emit(
+            {
+                "timestamp": datetime(2018, 3, 8, 11, 16, 10).isoformat() + "Z",
+                "from_self": False,
+                "from_jid": "romeo@montague.lit",
+                "display_name": "Romeo Montague",
+                "color_full": "#123456",
+                "color_weak": "#123",
+                "attachments": [],
+                "body": "<em>foo</em>",
+                "message_uid": "message-1"
+            }
+        )
+        self.page.channel.on_message.emit(
+            {
+                "timestamp": datetime(2018, 3, 8, 11, 16, 15).isoformat() + "Z",
+                "from_self": False,
+                "from_jid": "romeo@montague.lit",
+                "display_name": "Romeo Montague",
+                "color_full": "#123456",
+                "color_weak": "#123",
+                "attachments": [],
+                "body": "<em>bar</em>",
+                "message_uid": "message-2"
+            }
+        )
+        self.assertSubtreeEqual(
+            etree.fromstring(
+                '<div xmlns="http://www.w3.org/1999/xhtml" id="messages">'
+                '<div class="message-block">'
+                '<div class="avatar"><img/></div>'
+                '<div class="from">Romeo Montague</div>'
+                '<div class="message-block-messages">'
+                '<div class="message">'
+                '<div class="timestamp">3/8/2018, 11:16:10 AM</div>'
+                '<div class="body"><div><em>foo</em></div></div>'
+                '</div>'
+                '<div class="message">'
+                '<div class="timestamp">'
+                '<span class="visual-hidden">11:16</span><span>:15</span>'
+                '</div>'
+                '<div class="body"><div><em>bar</em></div></div>'
+                '</div>'
+                '</div>'
+                '<div class="clearfix"></div>'
+                '</div>'
+                '</div>'
+            ),
+            run_coroutine(self._obtain_html(), timeout=20),
+            ignore_surplus_attr=True,
+        )
+
+    def test_new_block_on_from_jid_change(self):
+        self.page.channel.on_message.emit(
+            {
+                "timestamp": datetime(2018, 3, 8, 11, 16, 10).isoformat() + "Z",
+                "from_self": False,
+                "from_jid": "romeo@montague.lit",
+                "display_name": "Romeo Montague",
+                "color_full": "#123456",
+                "color_weak": "#123",
+                "attachments": [],
+                "body": "<em>foo</em>",
+                "message_uid": "message-1"
+            }
+        )
+        self.page.channel.on_message.emit(
+            {
+                "timestamp": datetime(2018, 3, 8, 11, 16, 15).isoformat() + "Z",
+                "from_self": False,
+                "from_jid": "romeo@montague.litX",
+                "display_name": "Romeo Montague",
+                "color_full": "#123456",
+                "color_weak": "#123",
+                "attachments": [],
+                "body": "<em>bar</em>",
+                "message_uid": "message-2"
+            }
+        )
+        self.assertSubtreeEqual(
+            etree.fromstring(
+                '<div xmlns="http://www.w3.org/1999/xhtml" id="messages">'
+                '<div class="message-block">'
+                '<div class="avatar"><img/></div>'
+                '<div class="from">Romeo Montague</div>'
+                '<div class="message-block-messages">'
+                '<div class="message">'
+                '<div class="timestamp">3/8/2018, 11:16:10 AM</div>'
+                '<div class="body"><div><em>foo</em></div></div>'
+                '</div>'
+                '</div>'
+                '<div class="clearfix"></div>'
+                '</div>'
+                '<div class="message-block">'
+                '<div class="avatar"><img/></div>'
+                '<div class="from">Romeo Montague</div>'
+                '<div class="message-block-messages">'
+                '<div class="message">'
+                '<div class="timestamp">'
+                '<span class="visual-hidden"></span><span>11:16:15</span>'
+                '</div>'
+                '<div class="body"><div><em>bar</em></div></div>'
+                '</div>'
+                '</div>'
+                '<div class="clearfix"></div>'
+                '</div>'
+                '</div>'
+            ),
+            run_coroutine(self._obtain_html(), timeout=20),
             ignore_surplus_attr=True,
         )
