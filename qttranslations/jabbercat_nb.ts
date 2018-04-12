cc829774f5cb878fbaa550134c7376b6adbe2884
Allan Nordhøy <epost@anotheragency.no>
2018-04-12 08:00:07 +0200

    Norwegian Bokmål translation added

---
+++ qttranslations/jabbercat_nb.ts
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="nb" sourcelanguage="">
+<context>
+    <name>dlg_account_manager</name>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="14"/>
+        <source>Account manager</source>
+        <translation>Kontobehandling</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="32"/>
+        <source>List of accounts. Press enter to edit selected account.</source>
+        <translation type="obsolete">Kontoliste. Trykk ⏎ for å redigere valgte kontoer.</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="125"/>
+        <source>Add existing account</source>
+        <translation>Bestehendes Legg til eksisterende konto</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="122"/>
+        <source>Add</source>
+        <translation>Legg til</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="78"/>
+        <source>Delete selected account</source>
+        <translation>Slett valgt konto</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="81"/>
+        <source>Delete</source>
+        <translation>Slett</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="104"/>
+        <source>Close account manager</source>
+        <translation>Lukk kontobehandling</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="107"/>
+        <source>Close</source>
+        <translation>Lukk</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="127"/>
+        <source>New account?</source>
+        <translation type="obsolete">Ny konto</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="130"/>
+        <source>Add a new account</source>
+        <translation type="obsolete">Legg til ny konto</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="140"/>
+        <source>Delete account</source>
+        <translation type="obsolete">Slett konto</translation>
+    </message>
+    <message>
+        <location filename="jabbercat/client.py" line="90"/>
+        <source>JID</source>
+        <translation>JID</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_account_manager.ui" line="32"/>
+        <source>List of accounts. Press enter to edit and space to enable or disable selected account.</source>
+        <translation>Liste over kontoer. Trykk ⏎ for å redigere og mellomrom for å skru på eller av valgt konto</translation>
+    </message>
+</context>
+<context>
+    <name>dlg_check_certificate</name>
+    <message>
+        <location filename="data/dlg_check_certificate.ui" line="14"/>
+        <source>Untrusted server certificate</source>
+        <translation>Ubetrodd tjenersertifikat</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_check_certificate.ui" line="17"/>
+        <source>Controls become available after 3 seconds.</source>
+        <translation>Kontroller blir tilgjengelige etter tre sekunder.</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_check_certificate.ui" line="69"/>
+        <source>Do you want to connect to the server?</source>
+        <translation>Ønsker du å koble til tjeneren?</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_check_certificate.ui" line="79"/>
+        <source>Always trust this certificate</source>
+        <translation>Alltid stol på dette sertifikatet</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_check_certificate.ui" line="101"/>
+        <source>Continue</source>
+        <translation>Fortsett</translation>
+    </message>
+    <message>
+        <location filename="data/dlg_check_certificate.ui" line="108"/>
+        <source>Abort connection</source>
+        <translation>Avbryt tilkobling</translation>
+    </message>
+</context>
+<context>
+    <name>roster_window</name>
+    <message>
+        <location filename="data/roster.ui" line="14"/>
+        <source>My little XMPP client</source>
+        <translation>Min lille XMPP-klient</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="51"/>
+        <source>Global online/offline switch.</source>
+        <translation>Global pålogget/frakoblet-bryter.</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="54"/>
+        <source>Online</source>
+        <translation>Pålogget</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="67"/>
+        <source>Presence state which is shown to your contacts.</source>
+        <translation>Tilstedeværelsesstatus vist til dine kontakter</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="74"/>
+        <source>Free for chat</source>
+        <translation>Ledig for sludring</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="79"/>
+        <source>Available</source>
+        <translation>Tilgjengelig</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="84"/>
+        <source>Away</source>
+        <translation>Borte</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="89"/>
+        <source>Not available</source>
+        <translation>Ikke tilgjengelig</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="94"/>
+        <source>Do not disturb</source>
+        <translation>Ikke forstyrr</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="120"/>
+        <source>Roster</source>
+        <translation>Kontaktliste</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="132"/>
+        <source>View</source>
+        <translation>Vis</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="139"/>
+        <source>Accounts</source>
+        <translation>Kontakter</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="150"/>
+        <source>New conversation?</source>
+        <translation>Ny samtale?</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="155"/>
+        <source>Join chatroom?</source>
+        <translation>Ta del i sludrerom?</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="165"/>
+        <source>Add contact?</source>
+        <translation>Legg til kontakt?</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="175"/>
+        <source>Add group chat?</source>
+        <translation>Legg til gruppesludring?</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="185"/>
+        <source>Quit</source>
+        <translation>Avslutt</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="193"/>
+        <source>Show offline users</source>
+        <translation>Vis frakoblede brukere</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="204"/>
+        <source>Show empty groups</source>
+        <translation>Vis tomme grupper</translation>
+    </message>
+    <message>
+        <location filename="data/roster.ui" line="209"/>
+        <source>Manage accounts?</source>
+        <translation>Behandle kontoer?</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="data/roster.ui" line="150"/>
+        <source>New conversation…</source>
+        <translation>Ny samtale…</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="data/roster.ui" line="155"/>
+        <source>Join chatroom…</source>
+        <translation>Ta del i sludrerom…</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="data/roster.ui" line="165"/>
+        <source>Add contact…</source>
+        <translation>Legg til kontakt…</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="data/roster.ui" line="175"/>
+        <source>Add group chat…</source>
+        <translation>Legg til gruppesludring…</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="data/roster.ui" line="209"/>
+        <source>Manage accounts…</source>
+        <translation>Behandle kontoer…</translation>
+    </message>
+</context>
+</TS>
